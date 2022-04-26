40dcdfc0829410459a5bb67556c5a235fa221319
Anemixomaet <alexsolver1993@gmail.com>
2022-04-26 01:50:10 -0500

    Agregar archivos de proyecto.

---
+++ AlexSolorzanoExamen/Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AlexSolorzanoExamen
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string user = txtUser.Text;
+            string pass = txtPass.Text;
+
+            if ((user == "Alex") && (pass == "12345"))
+            {
+                await Navigation.PushAsync(new Registro(user, pass));
+                
+            }
+            else
+            {
+                DisplayAlert("Alerta", "Credenciales Incorectas", "cerrar");
+            }
+
+        }
+    }
+}
---
+++ AlexSolorzanoExamen/Resumen.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AlexSolorzanoExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Resumen : ContentPage
+    {
+
+        public Resumen(string nombre, string user, string fina)
+        {
+            InitializeComponent();
+            lbluser1.Text =  user;
+            lbluser.Text = nombre;
+            txtFinal.Text = fina;
+
+        }
+
+    }
+}
